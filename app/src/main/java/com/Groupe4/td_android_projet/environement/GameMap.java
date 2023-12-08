18539d8c372017eca5a6114c70d14812d0c47f3c
wallerandL <wallerandlahaussois@gmail.com>
2023-12-08 08:50:46 +0100

    ajout fps dans GameLoop creation de MapManager

--- app/src/main/java/com/Groupe4/td_android_projet/environement/GameMap.java
+++ app/src/main/java/com/Groupe4/td_android_projet/environement/GameMap.java
@@ -2,19 +2,40 @@ package com.Groupe4.td_android_projet.environement;
 
 import android.graphics.Canvas;
 
+import com.Groupe4.td_android_projet.entites.enemies.Skeleton;
 import com.Groupe4.td_android_projet.helpers.GameConstants;
+
+import java.util.ArrayList;
 
 public class GameMap {
 
     private int[][] spriteIds;
+    private ArrayList<Skeleton> skeletonsArrayList;
 
-    public GameMap(int[][] spriteIds) {
+    public GameMap(int[][] spriteIds, ArrayList<Skeleton>skeletonsArrayList) {
         this.spriteIds = spriteIds;
+        this.skeletonsArrayList = skeletonsArrayList;
+    }
+
+    public ArrayList<Skeleton> getSkeletonsArrayList() {
+        return skeletonsArrayList;
     }
 
     public void draw(Canvas c) {
         for (int j = 0; j < spriteIds.length; j++)
             for (int i = 0; i < spriteIds[j].length; i++)
                 c.drawBitmap(Floor.OUTSIDE.getSprite(spriteIds[j][i]), i * GameConstants.Sprite.SIZE, j * GameConstants.Sprite.SIZE, null);
+    }
+
+    public int getArrayWidth() {
+        return spriteIds[0].length;
+    }
+
+    public int getArrayHeight() {
+        return spriteIds.length;
+    }
+
+    public int getSpriteID(int xIndex, int yIndex) {
+        return spriteIds[yIndex][xIndex];
     }
 }
